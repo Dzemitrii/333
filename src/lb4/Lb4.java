85e71b88c2c206197c5f6c6c91eab2da0d234bf0
20390 <20390@1k129s5.dune.edu>
2023-03-22 11:03:00 +0200

    333

---
+++ src/lb4/Lb4.java
@@ -0,0 +1,17 @@
+package lb4;
+public class Lb4 {
+    public static void main(String[] args) {
+System.out.println("Hello world");
+System.out.println("Hello wersg");
+System.out.println("Hello wsrhsjr");
+System.out.println("Hello wesag");
+System.out.println("Hello wsrhh");
+System.out.println("Hello wfdh");
+System.out.println("Hello wdfg");
+System.out.println("Hello fgijgf");
+
+
+                   
+    }
+    
+}
